fa8a3208eccf5eeb497c0b9d0b4fba0eaf27b896
Carlos Vasquez <carlos.vasquez@enapsys.com>
2021-09-03 22:07:07 -0600

    update env

--- env.example.php
+++ env.example.php
@@ -1,12 +1,21 @@
 <?php
 
 $variables = [
-    'DB_TYPE' => 'pgsql',
-    'DB_HOST' => 'localhost',
-    'DB_USERNAME' => 'postgres',
-    'DB_PASSWORD' => 'postgres',
-    'DB_NAME' => 'db',
-    'DB_PORT' => '5432',
+    // postgres
+//    'DB_TYPE' => 'pgsql',
+//    'DB_HOST' => 'localhost',
+//    'DB_USERNAME' => 'postgres',
+//    'DB_PASSWORD' => '@ERT*>O',
+//    'DB_NAME' => 'tareas',
+//    'DB_PORT' => '5432',
+
+    // oracle
+    'DB_TYPE' => 'oci',
+    'DB_HOST' => 'localhost/XE',
+    'DB_USERNAME' => 'system',
+    'DB_PASSWORD' => 'oracle',
+    'DB_NAME' => '',
+    'DB_PORT' => '1521',
 ];
 
 foreach ($variables as $key => $value) {
